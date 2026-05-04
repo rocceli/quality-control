c9d4f554e3e15119b3437e8e8d96be76d50fe2e3
sabs530 <allansabiri83@gmail.com>
2026-05-04 11:37:45 +0300

    error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
     { name: 'Home', href: '#home' },
     { name: 'Services', href: '#services' },
     { name: 'Calibration Labs', href: '#calibration-labs' },
-    { name: 'Non Destructive Testing', href: '#ndt-labs' }
+    { name: 'Non Destructive Testing', href: '#ndt-labs' },
     { name: 'Certifications', href: '#certifications' },
     { name: 'Blog', href : '#blog' },
     { name: 'Contact', href: '#contact' },
